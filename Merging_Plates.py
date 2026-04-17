bb8350fe53f5075c955eafb4a6e4013597f205ef
Danny-Vu <dannyvu24@gmail.com>
2026-04-17 11:02:41 -0400

    Merging_Plates.py

---
+++ Merging_Plates.py
@@ -0,0 +1,8 @@
+"""
+Merging plate signals from FLIPR Penta with chemical set
+Python
+"""
+
+import pandas as pd
+df = pd.read_excel('/content/drive/MyDrive/1029 Chemical Study - Sarah-3/Donor 1083/1083 Plate Maps P1-8.xlsx')
+df
